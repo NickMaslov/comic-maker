f84be8cbfe69040fc7ad9252de64ddd06a1b6b96
Mykola Maslov <kategorychnost@gmail.com>
2019-10-16 06:35:19 -0400

    created server, initial server structure, connected sevver and client side

---
+++ server/index.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const port = process.env.PORT || 8080;
+const app = express();
+
+// logging middleware
+app.use(morgan("dev"));
+
+//a Connect/Express middleware
+app.use(cors());
+
+// body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// auth and api routes
+app.use("/auth", require("./auth"));
+app.use("/api", require("./api"));
+
+// static file-serving middleware
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+// any remaining requests with an extension (.js, .css, etc.) send 404
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error("Not found");
+    err.status = 404;
+    next(err);
+  } else {
+    next();
+  }
+});
+
+// sends index.html
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public/index.html"));
+});
+
+// error handling endware
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || "Internal server error.");
+});
+
+app.listen(port, () => console.log(`Web service running on port ${port}`));
